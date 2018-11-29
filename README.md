aeb58b7f89e3b59f6b6bf85b8b2d16b1ae937bb5
Bruno Porto <brunotporto@gmail.com>
2018-11-28 22:08:46 -0200

    Implementa código para recompactar arquivos zip em arquivos menores.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# recompactador
+# Recompactador
+
+
+```bash
+ruby recompactador.rb zip_ou_pasta nome_prefixo_arquivo_destino tamanho_arquivo_gerado_em_mb
+```
+
+**zip_ou_pasta**
+(obrigatório):
+Caminho completo para o arquivo ZIP a ser recompactado ou caminho da pasta
+contendo os arquivos a serem recompactados
+
+**nome_prefixo_arquivo_destino**
+(padrão: nome do arquivo sem sua extensão ou nome pasta):
+Nome utilizado para gerar os novos arquivos compactados (não informe extensão)
+
+**tamanho_arquivo_gerado_em_mb**
+(padrão: 50mb):
+Tamanho máximo que cada novo arquivo compactado deverá ter.
+
+### Exemplo de Uso
+```bash
+ruby recompactador.rb xmls11.zip novos_xml 20
+```
+Resulta em novos_xml_1.zip, novos_xml_2.zip, ..., com no máximo 20mb cada
+
+```bash
+ruby recompactador.rb pastaxmls
+```
+Resulta em pastaxmls_1.zip, pastaxmls_2.zip, ..., com no máximo 50mb cada
+
+```bash
+ruby recompactador.rb pastaxmls xmls 100
+```
+Resulta em xmls_1.zip, xmls_2.zip, ..., com no máximo 100mb cada
---
+++ recompactador.rb
@@ -0,0 +1,149 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+
+gem 'rubyzip', path: 'vendor/rubyzip-1.2.2.gem'
+gem 'ruby-progressbar', path: 'vendor/progressbar-1.10.0.gem'
+
+require 'fileutils'
+require 'zip'
+require 'ruby-progressbar'
+
+in_zip_or_folder = ARGV[0]
+if in_zip_or_folder.nil? || (!File.file?(in_zip_or_folder) && !File.directory?(in_zip_or_folder))
+  puts "ERRO: Pasta ou Arquivo inválido!"
+  return
+end
+
+out_zip_name = ARGV[1]
+if out_zip_name.nil?
+  out_zip_name = File.basename(in_zip_or_folder, '.zip')
+  puts "Nome do arquivo destino não informado, setado como: #{out_zip_name}"
+end
+
+chunk_mb = ARGV[3]
+if chunk_mb.nil?
+  chunk_mb = 50
+  puts "Tamanho limite dos arquivos não informado, setado como: #{chunk_mb}mb"
+end
+
+base_dir = File.dirname(in_zip_or_folder)
+out_zip_path = File.join(base_dir, out_zip_name)
+chunk_mb = chunk_mb * (2 ** 20)
+
+def count_files_dir(dir_path)
+  Dir.glob(File.join(dir_path, '**', '*')).select do |file|
+    File.file?(file)
+  end.count
+end
+
+def progress_bar(title, total)
+  ProgressBar.create(
+      :format => '%a %B %p%% %t',
+      :title => title,
+      :total => total
+  )
+end
+
+def unzip_it(zip_path, destination_dir)
+  Zip.default_compression = Zlib::BEST_COMPRESSION
+  Zip::File.open(zip_path) do |zip_file|
+    zip_name = File.basename(zip_file.name)
+    progressbar = progress_bar("Descompactando #{zip_name}", zip_file.entries.size)
+    zip_file.each do |zip_entry|
+      next if zip_entry.name =~ /__MACOSX/ || zip_entry.name =~ /\.DS_Store/ || !zip_entry.file?
+      filename = File.basename(zip_entry.name)
+      entry_path = File.join(destination_dir, filename)
+      FileUtils.rm_r(entry_path) if File.file?(entry_path)
+      zip_entry.extract(entry_path)
+      progressbar.increment
+    end
+    progressbar.finish unless progressbar.finished?
+  end
+  destination_dir
+end
+
+def zip_it(zip_path, files)
+  FileUtils.rm_r(zip_path) if File.file?(zip_path)
+  Zip::File.open(zip_path, Zip::File::CREATE) do |zipfile|
+    zip_name = File.basename(zip_path)
+    progressbar = progress_bar("Compactando #{zip_name}", files.size)
+    files.each do |file_path|
+      next if file_path =~ /__MACOSX/ || file_path =~ /\.DS_Store/ || !File.file?(file_path)
+      entry_name = File.basename(file_path)
+      zipfile.add(entry_name, file_path)
+      progressbar.increment
+    end
+    progressbar.finish unless progressbar.finished?
+  end
+  zip_path
+end
+
+def finish_tmp_file(tmp_path, zip_path, stream_file = nil, bytes_limit = 0)
+  tmp_size = File.size(tmp_path)
+  if tmp_size >= bytes_limit
+    FileUtils.rm_r(zip_path) if File.file?(zip_path)
+    FileUtils.mv tmp_path, zip_path
+    FileUtils.rm_r(tmp_path) if File.file?(tmp_path)
+    stream_file.close if stream_file
+    return true
+  end
+  false
+end
+
+def add_file_to_stream(file_path, stream_file)
+  stream_file.put_next_entry File.basename(file_path)
+  stream_file.write File.read(file_path)
+end
+
+def package_it(out_zip_path, files_dir, split_in_bytes = nil)
+  return zip_it("#{out_zip_path}.zip", files_dir) if split_in_bytes.nil?
+
+  zip_counter = 0
+  zip_path = nil
+  tmp_path = nil
+  stream_file = nil
+  progressbar = progress_bar("...", count_files_dir(files_dir))
+  zip_pool = []
+
+  Dir.foreach(files_dir) do |file_name|
+    file_path = File.join(files_dir, file_name)
+    next if file_path =~ /__MACOSX/ || file_path =~ /\.DS_Store/ || !File.file?(file_path)
+
+    if zip_path.nil?
+      zip_counter += 1
+      zip_path = "#{out_zip_path}_#{zip_counter}.zip"
+      zip_dirname, zip_basename = ::File.split(zip_path)
+      zip_pool << zip_basename
+      tmp_path = Dir::Tmpname.create(zip_basename, zip_dirname) {}
+      progressbar.title = "Compactando #{File.basename(zip_path)}"
+      stream_file = Zip::OutputStream.new(tmp_path)
+    end
+
+    add_file_to_stream(file_path, stream_file)
+
+    if finish_tmp_file(tmp_path, zip_path, stream_file, split_in_bytes)
+      zip_path = nil
+    end
+
+    progressbar.increment
+  end
+
+  finish_tmp_file(tmp_path, zip_path, stream_file)
+  progressbar.finish if progressbar && !progressbar.finished?
+
+  if zip_pool.size > 0
+    puts "#{zip_pool.size} arquivos gerados:"
+    zip_pool.each {|name| puts "-- #{name}"}
+  end
+end
+
+if File.directory?(in_zip_or_folder)
+  package_it(out_zip_path, in_zip_or_folder, chunk_mb)
+else
+  base_name = File.basename(in_zip_or_folder, '.zip')
+  destionation_dir = File.join(base_dir, base_name)
+  FileUtils.mkpath(destionation_dir) unless File.directory?(destionation_dir)
+  unzip_it(in_zip_or_folder, destionation_dir)
+  package_it(out_zip_path, destionation_dir, chunk_mb)
+  FileUtils.rm_r(destionation_dir)
+end
